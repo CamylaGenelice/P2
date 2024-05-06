90f8b71470e30f43cd675f8e4096a5e18c04372b
Camyla Genelice <genelicecamyla@gmail.com>
2024-05-06 14:13:55 -0300

    1.3.8

--- Projeto_3/Historia.java
+++ Projeto_3/Historia.java
@@ -2,8 +2,7 @@
       
  * A classe Historia é onde vai rodar a história do jogo. 
  */
-import java.util.Scanner;
-import java.util.Random;
+
 public class Historia {
     public static void main(String[] args) {
         
